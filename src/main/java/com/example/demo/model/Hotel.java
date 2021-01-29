415b46468df75c8dd3c5e5a4d60074f1178cb44a
aftab1161 <aaftaaaab@gmail.com>
2021-01-29 19:35:33 +0530

    Models Added.

--- src/main/java/com/example/demo/model/Hotel.java
+++ src/main/java/com/example/demo/model/Hotel.java
@@ -1,4 +1,25 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "hotel")
 public class Hotel {
+    @Id
+    private String id;
+    private String name;
+    @Indexed(direction = IndexDirection.ASCENDING)
+    private int pricePerNight;
+    private Address address;
+    private List<Review> reviews;
 }
---
+++ src/main/java/com/example/demo/model/Review.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private String userName;
+    private int rating;
+    private boolean approved;
+}
